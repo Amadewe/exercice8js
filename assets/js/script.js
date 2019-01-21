db75e3abab51ef4be0558fd1a91b695747d9f882
Amadewe <dewevre.amandine@gmail.com>
2019-01-21 22:29:28 +0100

    exercice 8 en cours et à reprendre demain

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function myFunction() {
+var age = document.getElementById("age").value;
+if majeur = (age >= 18){
+	alert('Vous êtes majeur')//le visiteur a au moins 18 ans
+}
+
+
+
+// // on compare l'age : s'il est superieur ou egal à 18 ans, alors le visiteur est majeur
+//
+// var majeur = (age >= 18);
+// alert('Vous êtes majeurs : ' + majeur);     // on verifie que ça marche ^^
+// var mineur = !(age >= 18);
+// alert('Vous êtes mineur : ' + majeur);
