8533b6e980fe7275bfdf903e3d430f0d69e678bf
Amadewe <dewevre.amandine@gmail.com>
2019-01-22 09:42:54 +0100

    Fin de l'exercice 8

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,23 @@
-function myFunction() {
-var age = document.getElementById("age").value;
-if majeur = (age >= 18){
-	alert('Vous êtes majeur')//le visiteur a au moins 18 ans
+// function myFunction() {
+// var age = document.getElementById("age").value;
+// if age =(age >= 18){
+// var result = ('Vous êtes majeur');
+// alert(result);
+// } else age =(age < 18){
+// 	var result = ('Vous êtes mineur');
+// alert(result);
+// }
+// }
+
+function button() {
+var age1 = document.getElementById("age").value;
+if (age1 >= 18) {
+    alert('Vous êtes majeur');
 }
-
-
-
+else {
+    alert('Vous êtes mineur');
+}
+}
 // // on compare l'age : s'il est superieur ou egal à 18 ans, alors le visiteur est majeur
 //
 // var majeur = (age >= 18);
