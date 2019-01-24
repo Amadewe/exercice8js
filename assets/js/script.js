a1b7747a00ef07aa50013280ce77d9a6c37a2870
Amadewe <dewevre.amandine@gmail.com>
2019-01-24 16:12:10 +0100

    Correction de l'exercice 8

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,21 @@
-// function myFunction() {
-// var age = document.getElementById("age").value;
-// if age =(age >= 18){
-// var result = ('Vous êtes majeur');
-// alert(result);
-// } else age =(age < 18){
-// 	var result = ('Vous êtes mineur');
-// alert(result);
-// }
-// }
 
-// function button(){
-//   var regex = new RegExp ('/^\d{1,2}$/');
-//   var age = document.getElementById('age').value;
-// if(!isNaN(age) && (age >= 5) && (age <= 100))
-//      return true;
-// else
-//      return false;
-// }
-
-
-
-function age() {
-  var regex = new RegExp (/^[0-9]$/);
-var age1 = document.getElementById('age').value;
-if (age1 >= 18) {
-    alert('Vous êtes majeur');
-} else {
-    alert('Vous êtes mineur');
+//Je déclare ma fonction
+function getSee(){
+  var age = document.getElementById('age').value;
+//Je vérifie que l'âge est écrit en chiffres
+  if(isNaN(age) == false){
+    //Je vérifie que l'âge est infèrieur à 114 et supérieur à 0
+    if ( age < 114 && age > 0) {
+      // Vérifie la majorité
+      if (age >= 18){
+        alert('Vous êtes majeur');
+      }else{
+        alert('Vous êtes mineur');
+      }
+    }else {
+      alert('Age non valide');
+    }
+  }else{
+    alert('En nombres stp');
+  }
 }
-}
-
-
-// // on compare l'age : s'il est superieur ou egal à 18 ans, alors le visiteur est majeur
-//
-// var majeur = (age >= 18);
-// alert('Vous êtes majeurs : ' + majeur);     // on verifie que ça marche ^^
-// var mineur = !(age >= 18);
-// alert('Vous êtes mineur : ' + majeur);
